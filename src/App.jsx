2d24c26fe39a9df6dc3418d4cde40d6ae5e00809
anthonychan1211 <anthonychan1211@gmail.com>
2023-07-20 11:06:24 +0100

    container added

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,27 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState({});
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(
         "https://www.superheroapi.com/api.php/10226261791523661/search/ironman"
       );
       const result = await res.json();
-      setData(result);
+      setData(result.results[0]);
     }
     fetchData();
-  });
+  }, []);
   console.log(data);
-  return <></>;
+  return (
+    <div className="page-container">
+      <div className="info">
+        <h2>Name : {data.biography["full-name"]}</h2>
+      </div>
+      <img src={data.image.url} alt="" className="avatar" />
+      <div className="stat"></div>
+    </div>
+  );
 }
 
 export default App;
