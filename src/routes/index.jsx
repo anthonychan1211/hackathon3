eac4b9688c5fafefd718287c73d6d253a2d7f2a8
anthonychan1211 <anthonychan1211@gmail.com>
2023-07-20 14:20:57 +0100

    nav structure changed

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,9 +2,16 @@
 
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../contexts";
-
+import NavBar from "../components/NavBar";
 export default function ProtectedRoute({ redirectTo }) {
   const { user } = useAuth();
 
-  return user ? <Outlet /> : <Navigate to={redirectTo} />;
+  return user ? (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  ) : (
+    <Navigate to={redirectTo} />
+  );
 }
