d5190cc912bd62b444b829e174e80dceb2818218
Nastasia777 <kuznenastia@gmail.com>
2023-07-20 13:01:08 +0100

    changes are made

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./routes";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Message from "./pages/Message";
 import { AuthProvider } from "./contexts";
 import "./App.css";
 
@@ -14,56 +15,10 @@ function App() {
           <Route index element={<Home />}></Route>
         </Route>
         <Route path="login" element={<Login />}></Route>
+        <Route path="/message" element={<Message />} />
       </Routes>
     </AuthProvider>
   );
 }
 
 export default App;
-
-// import React from "react";
-// import { Routes, Route } from "react-router-dom";
-// import ProtectedRoute from "./routes";
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import { AuthProvider } from "./contexts";
-// import "./App.css";
-
-// function App() {
-//   const [data, setData] = useState({});
-//   useEffect(() => {
-//     async function fetchData() {
-//       const res = await fetch(
-//         "https://www.superheroapi.com/api.php/10226261791523661/search/ironman"
-//       );
-//       const result = await res.json();
-//       setData(result.results[0]);
-//     }
-//     fetchData();
-//   }, []);
-//   console.log(data);
-//   return (
-//     Object.keys(data).length && (
-//       <div className="page-container">
-//         <div className="info">
-//           <table>
-//             <tbody>
-//               <tr>
-//                 <td>Hero Name:</td>
-//                 <td>{data.name}</td>
-//               </tr>
-//               <tr>
-//                 <td>Original Name:</td>
-//                 <td>{data.biography["full-name"]}</td>
-//               </tr>
-//             </tbody>
-//           </table>
-//         </div>
-//         <img src={data.image.url} alt="" className="avatar" />
-//         <div className="stat"></div>
-//       </div>
-//     )
-//   );
-// }
-
-// export default App;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuth } from "../../contexts";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   const { user } = useAuth();
@@ -19,6 +20,9 @@ export default function Home() {
   return (
     <>
       <h1>Hello {user}!</h1>
+      <div>
+        <Link to="/message">Message me</Link>
+      </div>
       {Object.keys(data).length > 0 && ( // Check if data is not empty before rendering
         <div className="page-container">
           <div className="info">
@@ -42,40 +46,3 @@ export default function Home() {
     </>
   );
 }
-
-// function App() {
-//   const [data, setData] = useState({});
-//   useEffect(() => {
-//     async function fetchData() {
-//       const res = await fetch(
-//         "https://www.superheroapi.com/api.php/10226261791523661/search/ironman"
-//       );
-//       const result = await res.json();
-//       setData(result.results[0]);
-//     }
-//     fetchData();
-//   }, []);
-//   console.log(data);
-//   return (
-//     Object.keys(data).length && (
-//       <div className="page-container">
-//         <div className="info">
-//           <table>
-//             <tbody>
-//               <tr>
-//                 <td>Hero Name:</td>
-//                 <td>{data.name}</td>
-//               </tr>
-//               <tr>
-//                 <td>Original Name:</td>
-//                 <td>{data.biography["full-name"]}</td>
-//               </tr>
-//             </tbody>
-//           </table>
-//         </div>
-//         <img src={data.image.url} alt="" className="avatar" />
-//         <div className="stat"></div>
-//       </div>
-//     )
-//   );
-// }
